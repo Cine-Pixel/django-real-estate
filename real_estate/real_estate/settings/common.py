52200c1e129f1c25037433072d47b41c321f0c79
Cine-Pixel <ikaumphriani@gmail.com>
2023-01-14 19:48:59 +0400

    refactor(): split settings module

--- real_estate/real_estate/settings/common.py
+++ real_estate/real_estate/settings/common.py
@@ -13,17 +13,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*_f!7i-zngj**k7f%-e$rn-o%+ipd$gl9+$y51lse^z(vp_e@i"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []  # type: ignore
 
@@ -73,17 +68,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "real_estate.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
