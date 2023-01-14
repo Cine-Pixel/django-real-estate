1daab698d1672731e841ba09d518b4b30cc92eb1
Cine-Pixel <ikaumphriani@gmail.com>
2023-01-14 19:54:15 +0400

    ci(flake8): fix flake8

--- real_estate/real_estate/settings/dev.py
+++ real_estate/real_estate/settings/dev.py
@@ -1,5 +1,4 @@
-from .common import *
-
+from .common import *  # noqa: F403
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-*_f!7i-zngj**k7f%-e$rn-o%+ipd$gl9+$y51lse^z(vp_e@i"
@@ -14,6 +13,6 @@ DEBUG = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
     }
 }
