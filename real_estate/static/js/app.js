a3edfcdea032e1da5bf51a4d6fbca497ff0b35e9
Cine-Pixel <ikaumphriani@gmail.com>
2023-01-21 16:04:55 +0400

    feat(): better navbar

--- real_estate/static/js/app.js
+++ real_estate/static/js/app.js
@@ -77,3 +77,34 @@ function addMarker(lat, lng) {
     map.flyTo([lat, lng], 16);
     marker.setLatLng([lat, lng]);
 }
+
+
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function logout() {
+    const url = "/logout/";
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: {
+            'csrfmiddlewaretoken': getCookie('csrftoken'),
+        },
+        success: function(response) {
+            location.reload();
+        }
+    });
+}
